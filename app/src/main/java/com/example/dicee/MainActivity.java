c9944a12a17fa9bc7ac213a46903d11cbaa38c16
RunnerM <pentek.marci@gmail.com>
2019-05-31 17:38:28 +0200

    may 31 16:35

--- app/src/main/java/com/example/dicee/MainActivity.java
+++ app/src/main/java/com/example/dicee/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button rollButton =  findViewById(R.id.rollButton);
+        final ImageView leftDice =  findViewById(R.id.image_leftDice);
+        final ImageView rightDice =  findViewById(R.id.image_rightDice);
+
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6,
+        };
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Dicee", "Roll button pressed");
+
+                Random randomNumberGen = new Random();
+                int number = randomNumberGen.nextInt(6);
+                Log.d("Dicee", "The number is: " + number);
+                int number2 = randomNumberGen.nextInt(6);
+                leftDice.setImageResource(diceArray[number]);
+                rightDice.setImageResource(diceArray[number2]);
+
+            }
+        });
     }
 }
